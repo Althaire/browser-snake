01aff8e17d9c7da8cfe029d07978e01d87f32ccb
Pilar Huidobro <huidobropilar@gmail.com>
2016-08-27 13:54:12 +0200

    Add stylesheet and draw snake

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ var context = canvasEl.getContext('2d');
 // To draw a rectangle: (pos x, pos y, width, height)
 // context.fillRect(0, 0, 250, 250);
 
+
+//
 var GRID_SIZE = 20; //All caps for emphasis on this constant
 for (var i = GRID_SIZE; i < canvasEl.width; i += GRID_SIZE) {
-  context.strokeStyle = 'white';
+  context.strokeStyle = 'black';
   context.beginPath();
   context.moveTo(i - 0.5, 0);
   context.lineTo(i - 0.5, canvasEl.height);
@@ -25,5 +27,38 @@ for (var i = GRID_SIZE; i < canvasEl.width; i += GRID_SIZE) {
   context.lineTo(canvasEl.width, i - 0.5);
   context.closePath();
   context.stroke();
-
 }
+
+//Snake snake |__|˚``
+var SNAKE_COLOR = 'coral';
+var SNAKE = [
+  { // Footer
+    x: 0,
+    y: 0
+  },
+  {
+    x: GRID_SIZE,
+    y: 0
+  },
+  {
+    x: GRID_SIZE * 2,
+    y: 0
+  },
+  { // Head
+    x: GRID_SIZE * 3,
+    y: 0
+  }
+];
+
+
+context.fillStyle = SNAKE_COLOR;
+SNAKE.forEach(function(snakePart) {
+  context.fillRect(snakePart.x, snakePart.y, GRID_SIZE, GRID_SIZE);
+}); // This draws Snake  |__|ˆ``
+
+var gameSpeed = 250; //in ms
+var snakeDirection = 'right';
+function gameManager() {
+  
+}
+var gameLoop = setInterval(gameManager, gameSpeed);
